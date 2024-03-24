323b7058f330ad692ac2caed56f9abdc6debcebf
jinhuxin <161416143+jinhuxin@users.noreply.github.com>
2024-03-24 13:03:53 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-597156ff-a19e-4247-9259-77cfa4b6995b/)
 
 
 echo "$curl_output" >> Update.tsx
